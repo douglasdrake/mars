c3b52431e57d7a3b2b103416ce3d18a429a20a29
Douglas Drake <dougdrake42@gmail.com>
2019-05-18 02:52:33 -0500

    Initial commit

---
+++ mars_urls.py
@@ -0,0 +1,14 @@
+# NASA URL
+NASA_URL = "https://mars.nasa.gov"
+NASA_NEWS_URL = "https://mars.nasa.gov/news"
+
+# JPL URLs
+JPL_URL = "https://www.jpl.nasa.gov"
+JPL_MARS_URL = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+
+
+# Mars facts URL
+MARS_FACTS_URL = "https://space-facts.com/mars/"
+
+# Images for Mars hemispheres
+MARS_HEMISPHERES = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
